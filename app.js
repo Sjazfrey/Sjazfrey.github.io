cda5d95bec0bb6b5712e2a9c7b2c8ae92ef59c75
Shirley Kaffrey <jazfrey@gmail.com>
2020-06-18 23:09:18 -0600

    project

--- app.js
+++ app.js
@@ -5,19 +5,38 @@ let alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 // 2) Click r2c2 -- it flips and we see 'A' -- match
 //    2a) Alert the user they got a match
 //    2b) Fade to different color, removing the letter
-
+       $(function(){
+        let row = 6;
+        let column = 6;
+        let alphabet = (row * column) / 2;
+        this.getRows = function(){return row}
+        this.getColumns = function() {return column}
+        this.getMatches = function () {return alphabet}
+})
+        let pickCard = false;
+        let newCard = null;
+        let newPairs = alphabet;
 // Game Play (Unmatch)
 // 1)  r1c1 -- it flips and we see 'A'
 // 2)  r2c2 -- it flips and we see 'B'
 //    2a) Alert the use it's not a match
 //    2b) Cards flip back over
-
+// $('td').on("click", function(event){
+//     ($(event.currentTarget).toggleClass('flipped'))
+//     $("td").on("click", function(){
+//         setTimeout(2000);
+//     })
+//  })
+ 
+//});
 // Click, click
 // Match or not a match
-//set up game upon visit
+//.addEventListner()
 
-// Fisher-Yates (aka Knuth) Shuffle
-function shuffle(array) {
+
+
+// Fisher-Yates (aka Knuth) Shuffle     Should this shuffle upon start of game??
+ function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
   
     while (currentIndex !== 0) {
@@ -30,7 +49,7 @@ function shuffle(array) {
     return array;
 }
 
-$(function(){
+ $(function(){
       
     // while (a = table.rows -column)
     alphabet = shuffle(alphabet);
@@ -40,16 +59,22 @@ $(function(){
         //iterate through columns
             let rowColumn = ".row" + (i + 1) + "-column" + (j + 1)
             //columns would be accessed using the "col" variable assigned in the for loop
-            $(rowColumn).text(alphabet[i * 6 + j])
+            //$(rowColumn).text(alphabet[i * 6 + j])
+            $(rowColumn).append ($("<div>").text(alphabet[i * 6 + j]))
+
+
+            //$(rowColumn).append(alphabet[i * 6 + j])
         }  
     }
-
     $('td').on("click", function(event){
-       ($(event.currentTarget).toggleClass('flipped'))
-    })
+        ($(event.currentTarget).toggleClass('flipped'))
+        $("td").on("click", function(){
+            setTimeout(2000);
+        })
+     })
+     
+
 })
 
-  //flip a card
-function flip() {
-    $('.card').toggleClass('flipped');
-}
+ 
+      
