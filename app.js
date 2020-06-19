79c45dcfeb6bd6b6e29e09dfa6e2b1ffea375f6a
Shirley Kaffrey <jazfrey@gmail.com>
2020-06-19 13:43:32 -0600

    Works again

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
 let alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'] // Alphabet A-Z 
 
 // Game Play (Match)
-// 1) Click r1c1 -- it flips and we see 'A'
-// 2) Click r2c2 -- it flips and we see 'A' -- match
+
 //    2a) Alert the user they got a match
 //    2b) Fade to different color, removing the letter
-       $(function(){
-        let row = 6;
-        let column = 6;
-        let alphabet = (row * column) / 2;
-        this.getRows = function(){return row}
-        this.getColumns = function() {return column}
-        this.getMatches = function () {return alphabet}
-})
-        let pickCard = false;
-        let newCard = null;
-        let newPairs = alphabet;
+       
 // Game Play (Unmatch)
-// 1)  r1c1 -- it flips and we see 'A'
-// 2)  r2c2 -- it flips and we see 'B'
 //    2a) Alert the use it's not a match
 //    2b) Cards flip back over
-// $('td').on("click", function(event){
-//     ($(event.currentTarget).toggleClass('flipped'))
-//     $("td").on("click", function(){
-//         setTimeout(2000);
-//     })
-//  })
- 
-//})
+
 
 // Fisher-Yates (aka Knuth) Shuffle    This shuffle upon start of game??
  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
   
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
@@ -51,7 +31,7 @@ let alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
         for (let j = 0; j < 6; j++) {        //iterate through columns
             let rowColumn = ".row" + (i + 1) + "-column" + (j + 1) + "> .back" 
             //.row1-column1 > .back         
-            $(rowColumn).append ($("<div>").text(alphabet[i * 6 + j]))
+            $(rowColumn).text(alphabet[i * 6 + j])
         }  
     }
     $('td').on("click", function(event){
@@ -59,8 +39,26 @@ let alphabet = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
         let clickClass = ($(event.currentTarget).attr('class').split(' ')[0]);
         $("." + clickClass).toggleClass('flipped')
         console.log(clickClass)
-     })
-     
+        let rowColumn = "." + clickClass + "> .back" // .row1-column1 > .back
+        let clickLetter = $(rowColumn).text()        
+        console.log(clickLetter)
+        box.push(clickLetter) 
+        console.log(box)// [B, O] 
+        if (box.length == 2){
+            if (box[0] == box[1]) {
+                console.log(true)        
+            }  
+            else {
+                setTimeout(function(){
+                    $("td").removeClass('flipped')
+                }, 1500)
+
+                console.log(false)
+            }
+            box = []
+        }
+    })
+     let box = [] // [N, M]
 })
 
  
